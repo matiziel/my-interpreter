5d3bd534d30e52dc5affe69a95529936c24a5a11
Mateusz <matiziel10@gmail.com>
2020-04-03 00:36:08 +0200

    First commit, add console app and unit test project

---
+++ MyInterpreter/MyInterpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
